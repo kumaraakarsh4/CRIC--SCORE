090823d141f0121d72863717cf11f493031cf02d
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-11-09 00:26:36 +0530

    code updation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,65 @@
 import React from 'react'
+
 import { headerStyles } from '../assets/dummyStyles'
+
 import logo from '../assets/logo.png'
 
+
+
 const Header = () => {
+
   return (
+
    <header className={headerStyles.container}>
+
     <div className={headerStyles.innerContainer}>
+
+        <div className={headerStyles.mainWrapper}>
+
+            <div className={headerStyles.logoContainer}>
+
+                <div className={headerStyles.logoImage}>
+
+                    <img src={logo} alt="Logo" className={headerStyles.logoImg} />
+
+
+
+                </div>
+
+                <div className={headerStyles.logoText}>
+
+                    <div className={headerStyles.logoTitle} style={{
+
+                        fontFamily : "'Eczar' serif"
+
+                    }}>
+
+                        Cricscore
+
+
+
+                    </div>
+
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+
 
     </div>
 
+
+
    </header>
+
   )
+
 }
+
+
 
 export default Header
