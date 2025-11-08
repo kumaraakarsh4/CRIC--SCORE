44c410d6ab99b950faf794c3a98c412e8c5f2577
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-11-09 00:02:54 +0530

    code updation

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { headerStyles } from '../assets/dummyStyles'
+import logo from '../assets/logo.png'
+
+const Header = () => {
+  return (
+   <header className={headerStyles.container}>
+    <div className={headerStyles.innerContainer}>
+
+    </div>
+
+   </header>
+  )
+}
+
+export default Header
