69ce078f353be4486ab0d5f75bffd4ec41387443
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-11-10 22:59:06 +0530

    code updation

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import {homeStyles} from "../assets/dummyStyles"
 import Header from '../components/Header'
+import Footer from '../components/Footer';
 const Home = () => {
+  const heroWrapperStyle = {
+    perspective: '1100px',
+    WebkitPerspective: '1100px',
+  };
+
+  const heroBoxStyle = {
+    transformStyle: 'preserve-3d',
+    WebkitTransformStyle: 'preserve-3d',
+  };
   return (
     <div className={homeStyles.root}>
         <div className={homeStyles.blob1}
@@ -22,9 +32,31 @@ const Home = () => {
             />
 
         </div>
+        <main className={homeStyles.main}>
+          <section className={homeStyles.section}>
+            <div className={homeStyles.heroWrapper} style={heroWrapperStyle}>
+              <div className={homeStyles.heroBox} style={heroBoxStyle}>
+                <div className={homeStyles.heroSpotlight} style={{background: homeStyles.heroSpotlightGradient}}>
 
+                </div>
+                <div className={homeStyles.heroContent}>
+                  <div className={homeStyles.heroText}>
+                    <h1 className={homeStyles.heroTitle} style={{fontFamily:"'Poppins', system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', Arial"}}>
+                  Follow every match <br /> Real Time score , classy insights.
+                    </h1>
+                  </div>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </section>
+
+        </main>
     </div>
-  )
-}
+  );
+};
 
 export default Home
